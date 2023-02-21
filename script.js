a7b53390b14fd623df51fa7c8e7c765a0fa71f4d
Joo <joomartinimre@gmail.com>
2023-02-21 12:28:17 +0100

    struktura kiepitese

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+var kartyaAdatok=[
+    {id:1,value:3,sign:''},
+    {id:2,value:4,sign:''},
+    {id:3,value:5,sign:''},
+    {id:4,value:2,sign:''},
+    {id:5,value:-3,sign:''},
+    {id:6,value:2,sign:''},
+    {id:7,value:0,sign:'taliga'},
+    {id:8,value:6,sign:''},
+    {id:9,value:-6,sign:''},
+    {id:10,value:0,sign:'pap'},
+    {id:11,value:5,sign:''},
+    {id:12,value:0,sign:'sarkany'},
+    {id:13,value:4,sign:''},
+    {id:14,value:-5,sign:''},
+    {id:15,value:0,sign:'hegy'},
+    {id:16,value:-2,sign:''},
+    {id:17,value:-1,sign:''},
+    {id:18,value:1,sign:''},
+    {id:19,value:-4,sign:''},
+    {id:20,value:6,sign:''},
+    {id:21,value:0,sign:'hegy'},
+    {id:22,value:3,sign:''},
+    {id:23,value:1,sign:''},
+
+]
+
+var varAdatok = [
+    {id:1,value:1,color:'kek'},
+    {id:2,value:2,color:'kek'},
+    {id:3,value:3,color:'kek'},
+    {id:4,value:4,color:'kek'},
+    {id:5,value:1,color:'piros'},
+    {id:6,value:2,color:'piros'},
+    {id:7,value:3,color:'piros'},
+    {id:8,value:4,color:'piros'},
+    {id:9,value:1,color:'sarga'},
+    {id:10,value:2,color:'sarga'},
+    {id:11,value:3,color:'sarga'},
+    {id:12,value:4,color:'sarga'},
+    {id:13,value:1,color:'zold'},
+    {id:14,value:2,color:'zold'},
+    {id:15,value:3,color:'zold'},
+    {id:16,value:4,color:'zold'},
+
+]
 var eredmeny = document.getElementById("eredmeny");
 var tabla = document.createElement("div");
 var kartyaBox = document.createElement("div");
@@ -45,103 +91,13 @@ function Tablageneralas()
     }
 }
 
-function Tombfeltoltes(tomb, db, n)
-{
-    while (tomb.length < db)
-    {
-        var a = Math.floor(Math.random() * n) + 1;
-        if (tomb.indexOf(a) === -1) {
-            tomb.push(a);
-        }
-    }
-}
-
-function ToronyFeltolt(tomb,tomb2, db, n)
-{
-    elltomb = [];
-    while (tomb.length < db)
-    {
-        var a = Math.floor(Math.random() * n) + 1;
-        var b = Math.floor(Math.random() * n) + 1;
-        if (elltomb.indexOf(String(a)+String(b)) === -1) {
-            elltomb.push(String(a)+String(b));
-            tomb.push(a);
-            tomb2.push(b);
-        }
-        
-    }
-    console.log(elltomb);
-}
-
-function KartyakatTablabaGeneral(db)
-{
-    //Egy kártyát kiválaszt és elhelyez az első mezőben
-    //A kártya elhelyezése egy véletlen helyre
-    //db képet helyezzen el véletlen helyre
-    //nem ismétlődhet a kép ls a hely sem
-    var tomb = [];
-    Tombfeltoltes(tomb, db, 23);
-    var tomb2 = [];
-    Tombfeltoltes(tomb2, 30, 30);
-    for (var i = 0; i < db; i++)
-    {  
-        var img = document.createElement("img");
-        img.src = "img/"+(tomb[i])+".png";
-        var hely = document.getElementById(tomb2[i]-1);
-        hely.appendChild(img)
-    }
-    var ttomb =[];
-    var ttomb2 =[];
-    ToronyFeltolt(ttomb, ttomb2, 7, 4);
-    for (var i = 0; i < ttomb.length; i++)
-    {  
-        var img = document.createElement("img");
-        if (ttomb[i] == 1)
-        {
-            img.src = "img/kekvar"+(ttomb2[i])+".png";
-        }
-        else if (ttomb[i] == 2)
-        {
-            img.src = "img/pirosvar"+(ttomb2[i])+".png";
-        }
-        else if (ttomb[i] == 3)
-        {
-            img.src = "img/zoldvar"+(ttomb2[i])+".png";
-        }
-        else if (ttomb[i] == 4)
-        {
-            img.src = "img/sargavar"+(ttomb2[i])+".png";
-        }
-        var hely = document.getElementById(tomb2[i+23]-1);
-        hely.appendChild(img)
-    }
-}
-
-function CoinGeneralas(){
-    for (let index = 1; index <= 4; index++) {
-        let div = document.createElement("div");
-        for (let i = 1; i <= 3; i++) {
-           
-            var img = document.createElement("img");
-            img.src = "img/forint"+(index)+".png";
-            img.style.height = 4+"vw";
-            img.style.width = 4+"vw";
-            pontBox.appendChild(div);
-            div.appendChild(img);
-            
-            
-        }
-        
-    }
-    
-}
 
 function Main()
 {
+
     EredmenyBetoltes();
     Elrendezes();
     Tablageneralas();
-    KartyakatTablabaGeneral(23);
-    CoinGeneralas();
+
 }    
 Main();
